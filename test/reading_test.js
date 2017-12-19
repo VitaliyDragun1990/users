015f430bda8a9bc3fa888d8d54d9ff4bc2459e7e
Vitaly Dragun <vdrag00n90@gmail.com>
2017-12-19 16:16:23 +0200

    MongoDB Developers Guide project - final commit for test project

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,11 +2,16 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-    let joe;
+    let joe, maria, alex, zach;
 
     beforeEach((done) => {
+        // create users object
+        alex = new User({name: 'Alex'});
         joe = new User({name: 'Joe'});
-        joe.save()
+        maria = new User({name: 'Maria'});
+        zach = new User({name: 'Zach'});
+        // save them all to database
+        Promise.all([alex.save(), joe.save(), maria.save(), zach.save()])
             .then(() => done());
     });
 
@@ -20,10 +25,23 @@ describe('Reading users out of the database', () => {
     });
 
     it('finds a user with a particular id', (done) => {
-       User.findOne({ _id: joe._id })
-           .then((user) => {
+        User.findOne({_id: joe._id})
+            .then((user) => {
                 assert(user.name === 'Joe');
                 done();
-           });
+            });
+    });
+
+    it('can skip and limit the result set', (done) => {
+        User.find({})           // find all users in 'users' collection in database
+            .sort({name: 1})    // sort all users by 'name' property ascending fashion
+            .skip(1)            // skip 1 user in result
+            .limit(2)           // limit the result list to 2 users
+            .then((users) => {
+                assert(users.length === 2);     // make appropriate assertions
+                assert(users[0].name === 'Joe');
+                assert(users[1].name === 'Maria');
+                done();
+            });
     });
 });
